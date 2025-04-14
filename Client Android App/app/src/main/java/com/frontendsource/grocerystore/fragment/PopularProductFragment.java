00af04a047bc969a14d17013383e9c9fa0e4a78b
Ramadhani-Yassin <yasynramah@gmail.com>
2025-04-14 23:28:25 +0300

    okay

--- Client Android App/app/src/main/java/com/frontendsource/grocerystore/fragment/PopularProductFragment.java
+++ Client Android App/app/src/main/java/com/frontendsource/grocerystore/fragment/PopularProductFragment.java
@@ -74,7 +74,7 @@ public class PopularProductFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
-        getActivity().setTitle("Popular Product");
+        getActivity().setTitle("Popular Products");
     }
 
     private void getPopularProduct() {
